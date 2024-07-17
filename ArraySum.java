ff543e33a02c82560be3849a36094ae0d8efb048
Ankit singh <singhankit0460@gmail.com>
2024-07-17 23:55:30 +0530

    new

---
+++ ArraySum.java
@@ -0,0 +1,12 @@
+public class ArraySum {
+  public static void main(String[] args) {
+      int[] numbers = {10, 20, 30, 40, 50};
+      int sum = 0;
+
+      for (int i = 0; i < numbers.length; i++) {
+          sum += numbers[i];
+      }
+
+      System.out.println("The sum of the array elements is: " + sum);
+  }
+}
